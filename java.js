bb8ee6f66c24c1cd4fe9179e13e134a4391c109a
NinKyuWudd <72558822+ReihanWudd@users.noreply.github.com>
2021-08-15 08:06:42 -0700

    update java.js
    menambahkan fungsi untuk membuka gambar
    menambahakan fungsi pada tanda X untuk close gambar

--- java.js
+++ java.js
@@ -23,3 +23,16 @@ function fungsinav(){
         navbar.classList.remove("sticky")
     }
 }
+
+
+var fullimagebox = document.getElementById("fullimagebox")
+var fullimage = document.getElementById("fullimage")
+
+function openfullimage(pic){
+    fullimagebox.style.display = "flex"
+    fullimage.src= pic;
+}
+
+function closefullimage(){
+    fullimagebox.style.display = "none";
+}
